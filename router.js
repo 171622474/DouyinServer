20a78cc6b96096433a05e92b618d8a70cde82191
zhangchao <zhangchao@behe.com>
2020-03-09 00:10:11 +0800

    更改策略

--- router.js
+++ router.js
@@ -19,12 +19,43 @@ function MyReplace(data){
 }
 
 router.post('/api/douyin',async (ctx,next)=>{
+    console.log(ctx.request.body.url)
+    
     try{
-        const res = await axios.get(ctx.request.body.url)
-        var reg = /(?<=playAddr: ").*?(?=")/ //匹配
-        var resUrl = _.replace(reg.exec(res.data)[0],'playwm','play') //解析
-        let data = await MyReplace(resUrl)//匹配重定向模拟移动端隐藏地址
-        ctx.body = {code:200,data:data,msg:'解析成功'}
+        // const res = await axios.post('https://www.tingsang.com/ajax/analyze.php',{
+        //     link:ctx.request.body.url
+        // })
+        // console.log(res)
+        data = {
+            link:ctx.request.body.url
+        }
+        const res = await axios({
+            url:'https://www.tingsang.com/ajax/analyze.php',
+            data,
+            method:'post',
+            transformRequest: [function (data) {
+                let ret = ''
+                for (let it in data) {
+                  ret += encodeURIComponent(it) + '=' + encodeURIComponent(data[it]) + '&'
+                }
+                return ret
+              }],
+              headers: {
+                'Content-Type': 'application/x-www-form-urlencoded'
+              }
+        })
+        
+        
+        // var reg = /(?<=playAddr: ").*?(?=")/ //匹配
+        // var resUrl = _.replace(reg.exec(res.data)[0],'playwm','play') //解析
+        // let data = await MyReplace(resUrl)//匹配重定向模拟移动端隐藏地址
+        
+        
+        
+        ctx.body = {code:200,data:{url:res.data.data.video},msg:'解析成功'}
+
+
+
     }catch(err){
         ctx.body = {code:99999,data:{},msg:'地址可能错误'}
     }
